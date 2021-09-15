22279299ca390e7e22259bca27f30f940940a580
ddcbenc <ben.corneau@coxautoinc.com>
2021-09-14 20:18:44 -0400

    use exec

--- run.js
+++ run.js
@@ -1,30 +1,8 @@
 #!/usr/bin/env node
 
-const open = require('open');
-const path = require('path');
-const fs = require('fs');
+const {exec} = require("child_process");
+const puppeteer = require('puppeteer');
 
-const localChromeDir = path.join(__dirname, 'node_modules', 'puppeteer', '.local-chromium');
-
-function walk(dir){
-    for(let entry of fs.readdirSync(dir, {'withFileTypes':true})){
-        if(entry.name == 'Chromium.app' || entry.name == "Chromium.exe"){
-            return path.join(dir, entry.name);
-        }
-
-        if(!entry.name.startsWith(".") && entry.isDirectory()){
-            const res = walk(path.join(dir, entry.name));
-            if(res){
-                return res;
-            }
-        }
-    }
-}
-
-const appPath = walk(localChromeDir);
-if(appPath){
-    console.log(`starting ${appPath}`);
-    open(appPath);
-}else{
-    console.log(`unable to locate chromium application.  Search path:${localChromeDir}`);
-}
+const appPath = puppeteer.executablePath();
+console.log(`opening '${appPath}'...`);
+exec(appPath);
